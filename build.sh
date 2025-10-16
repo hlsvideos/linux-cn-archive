970abc49f64e9f9c9afa5089ace96d0c0ce707a1
hlsvideos <167844790+hlsvideos@users.noreply.github.com>
2025-10-16 09:01:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.126.2/hugo_extended_0
 tar xf hugo_extended_0.126.2_Linux-64bit.tar.gz
 time ./hugo --gc --minify
 ls
-rm -r ./public/data/attachment/album
+
+mkdir htmls
+tar -c public | zstd -c -T0 -level 10 | split -b 20m -d - htmls/linux-cn-archive-htmls.tar.zst.
+mv htmls public
 
 find ./public > find-public.txt
-rm -r public/tags
+rm -r ./public/data/attachment/album
+rm -r ./public/tags
 cp find-public.txt public
 mv find-public.txt public/index.txt
