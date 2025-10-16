1fa42823f5204514c212ae4ec21acb847796c471
hlsvideos <167844790+hlsvideos@users.noreply.github.com>
2025-10-16 06:27:05 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/gohugoio/hugo/releases/download/v0.126.2/hugo_extended_0.126.2_Linux-64bit.tar.gz
+tar xf hugo_extended_0.126.2_Linux-64bit.tar.gz
+./hugo --gc --minify
