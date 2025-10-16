e328ec8b757e5fa6a698bd19f750108011b86025
hlsvideos <167844790+hlsvideos@users.noreply.github.com>
2025-10-16 14:26:10 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,9 +13,10 @@ find ./public > find-public.txt
 #rm -r ./public/data/attachment/album
 rm -r ./public/data/attachment/album/201*
 rm -r ./public/data/attachment/album/2020*
-rm -r ./public/data/attachment/album/2021*
+rm -r ./public/data/attachment/album/2021
 rm -r ./public/data/attachment/album/2022
-rm -r ./public/data/attachment/album/2023
+rm -r ./public/data/attachment/album/2023*
+rm -r ./public/data/attachment/album/2024*
 rm -r ./public/tags
 find ./public > after-remove.txt
 cp find-public.txt public
