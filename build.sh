a83ad18c580f7abf13c8a307e4f0f89f64089d5e
hlsvideos <167844790+hlsvideos@users.noreply.github.com>
2025-10-16 08:09:20 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.126.2/hugo_extended_0
 tar xf hugo_extended_0.126.2_Linux-64bit.tar.gz
 time ./hugo --gc --minify
 ls
-find .
+rm -r ./public/data/attachment/album
